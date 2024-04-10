76e8266c9d2dd0d5890027f2cc9afccfe6a213c5
Hugo Culot <hugoculot.dev@gmail.com>
2024-04-10 15:09:11 +0200

    All path in changed world working.

--- changeWorld.js
+++ changeWorld.js
@@ -8,27 +8,40 @@ function checkWorldChange(worldCollisionBoard, tileSize) {
     for (let x = 0; x < currentLine.length; x++) {
       const currentTileValue = currentLine[x];
       if (currentTileValue === 300 && pointIsInRect(xHero -10 + wHero / 2, yHero -15 + hHero, tileSize * x, tileSize * y, tileSize, tileSize)) {
-        currentWorld = 0; // Le temple (Je sais pas pourquoi l'index 9 !!!)
+        // Code pour le changement de monde vers le temple
+        currentWorld = 0;
         xHero = 12 * worldsTileSizes[currentWorld];
         yHero = 6 * worldsTileSizes[currentWorld];
         return true;
-      } else if (currentTileValue === 301 && pointIsInRect(xHero -10 + wHero / 2, yHero -15 + hHero, tileSize * x, tileSize * y, tileSize, tileSize)) {
-        currentWorld = 1; // Le Village
-        xHero = 3 * worldsTileSizes[currentWorld]; 
-        yHero = 10 * worldsTileSizes[currentWorld];
+      } else if ((currentTileValue === 301 || currentTileValue === 305 || currentTileValue === 306) && pointIsInRect(xHero -10 + wHero / 2, yHero -15 + hHero, tileSize * x, tileSize * y, tileSize, tileSize)) {
+        // Code pour le changement de monde vers le village
+        currentWorld = 1;
+        if (currentTileValue === 301) {
+          xHero = 3 * worldsTileSizes[currentWorld]; 
+          yHero = 10 * worldsTileSizes[currentWorld];
+        } else if (currentTileValue === 305) {
+          xHero = 17 * worldsTileSizes[currentWorld]; 
+          yHero = 2 * worldsTileSizes[currentWorld];
+        } else if (currentTileValue === 306) {
+          xHero = 40 * worldsTileSizes[currentWorld]; 
+          yHero = 15 * worldsTileSizes[currentWorld];
+        }
         return true;
-      }else if (currentTileValue === 302 && pointIsInRect(xHero, yHero, tileSize * x, tileSize * y, tileSize, tileSize)) {
-        currentWorld = 2; // La Fôret
+      } else if (currentTileValue === 302  && pointIsInRect(xHero, yHero, tileSize * x, tileSize * y, tileSize, tileSize)) {
+        // Code pour le changement de monde vers la forêt
+        currentWorld = 2;
         xHero = 2 * worldsTileSizes[currentWorld];
         yHero = 9 * worldsTileSizes[currentWorld];
         return true;
       } else if (currentTileValue === 303 && pointIsInRect(xHero, yHero, tileSize * x, tileSize * y, tileSize, tileSize)) {
-        currentWorld = 3; // La Grotte
+        // Code pour le changement de monde vers la grotte
+        currentWorld = 3;
         xHero = 2 * worldsTileSizes[currentWorld]; 
         yHero = 9 * worldsTileSizes[currentWorld];
         return true;
       } else if (currentTileValue === 304 && pointIsInRect(xHero, yHero, tileSize * x, tileSize * y, tileSize, tileSize)) {
-        currentWorld = 4; // Le Boss
+        // Code pour le changement de monde vers le boss
+        currentWorld = 4;
         xHero = 2 * worldsTileSizes[currentWorld]; 
         yHero = 8 * worldsTileSizes[currentWorld];
         return true;
@@ -37,6 +50,7 @@ function checkWorldChange(worldCollisionBoard, tileSize) {
   }
   return false;
 };
+
 
 
 
