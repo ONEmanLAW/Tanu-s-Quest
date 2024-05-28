00bf5c73c174f77fd6c82cbabcf098a29ac472b2
Hugo Culot <hugoculot.dev@gmail.com>
2024-05-29 00:27:43 +0200

    Took off sounds of foot

--- sketch.js
+++ sketch.js
@@ -195,13 +195,13 @@ function setup() {
   soundHero = loadSound('hero.mp3');
   soundGrenouille = loadSound('frog.wav');
 
-  for (let i = 0; i < 2; i++) {
-    snowSounds[i] = loadSound(`snowSounds/stepsnow_${i + 1}.wav`); // Replace with the path to your snow sound files
-  }
+  // for (let i = 0; i < 2; i++) {
+  //   snowSounds[i] = loadSound(`snowSounds/stepsnow_${i + 1}.wav`); // Replace with the path to your snow sound files
+  // }
 
-  for (let i = 0; i < 8; i++) {
-    dirtSounds[i] = loadSound(`dirtSounds/stepdirt_${i + 1}.wav`); // Replace with the path to your snow sound files
-  }
+  // for (let i = 0; i < 8; i++) {
+  //   dirtSounds[i] = loadSound(`dirtSounds/stepdirt_${i + 1}.wav`); // Replace with the path to your snow sound files
+  // }
 
   musiqueBackgroundVillageBird = loadSound('birdSound.mp3');
  
@@ -240,10 +240,6 @@ let musiqueBackgroundBoss;
 
 function drawLoadingScreen() {
   image(loadingGif, 0, 0, width, height);
-  if (walkingSoundPlaying) {
-    currentWalkingSound.stop();
-    walkingSoundPlaying = false;
-  }
 };
 
 
@@ -268,8 +264,8 @@ function drawGame() {
     musiqueFond.setVolume(musicVolumeSlider.value());
     sounds.forEach(sound => sound.setVolume(musicVolumeSlider.value()));
     
-    snowSounds.forEach(snowSounds => snowSounds.setVolume(soundVolumeSlider.value()));
-    dirtSounds.forEach(dirtSounds => dirtSounds.setVolume(soundVolumeSlider.value()));
+    //snowSounds.forEach(snowSounds => snowSounds.setVolume(soundVolumeSlider.value()));
+    //dirtSounds.forEach(dirtSounds => dirtSounds.setVolume(soundVolumeSlider.value()));
     swordAndGrenouilleSound.setVolume(soundVolumeSlider.value());
     hitSound.setVolume(soundVolumeSlider.value());
     soundGrandSage.setVolume(soundVolumeSlider.value());
--- worlds/keysAndMovement.js
+++ worlds/keysAndMovement.js
@@ -110,11 +110,6 @@ function updateAnimation(animationArray) {
 
 let lastDirection = 'down';
 let lastHorizontalDirection = 'left';
-let snowSounds = [];
-let dirtSounds = [];
-let stoneSounds = [];
-let currentWalkingSound;
-let walkingSoundPlaying = false;
 
 function checkKeys(currentMap) {
   if (!gameOver) {
@@ -132,46 +127,21 @@ function checkKeys(currentMap) {
       }
     }
 
-    // Function to handle playing a random walking sound based on the current world
-    function playRandomWalkingSound() {
-      if (!walkingSoundPlaying) {
-        let soundArray;
-        if (currentWorld === 0) {
-          soundArray = snowSounds;
-        } else if (currentWorld === 1 || currentWorld === 5) {
-          soundArray = dirtSounds;
-        }
-        currentWalkingSound = random(soundArray);
-        currentWalkingSound.loop();
-        walkingSoundPlaying = true;
-      }
-    }
-
-    // Function to handle stopping the current walking sound
-    function stopWalkingSound() {
-      if (walkingSoundPlaying) {
-        currentWalkingSound.stop();
-        walkingSoundPlaying = false;
-      }
-    }
+   
 
     // Temple, Foret, Grotte, Boss
     if (currentMap === 0 || currentMap === 2 || currentMap === 3 || currentMap === 4) {
       if (keyIsDown(68) && keyIsDown(81)) {
-        stopWalkingSound();
         return;
       }
 
       if (keyIsDown(68)) {
         moveRight();
         lastHorizontalDirection = 'right';
-        playRandomWalkingSound();
       } else if (keyIsDown(81)) {
         moveLeft();
         lastHorizontalDirection = 'left';
-        playRandomWalkingSound();
       } else {
-        stopWalkingSound();
       }
 
       if (keyIsDown(32)) {
@@ -190,38 +160,33 @@ function checkKeys(currentMap) {
     // Village
     if (currentMap === 1 || currentMap === 5) {
       if (keyIsDown(68) && keyIsDown(81)) {
-        stopWalkingSound();
         return;
       }
 
       if (keyIsDown(90) && keyIsDown(83)) {
-        stopWalkingSound();
         return;
       }
 
       if (keyIsDown(68)) {
         moveRightVillage();
         lastDirection = 'right';
-        playRandomWalkingSound();
+ 
       } else if (keyIsDown(81)) {
         moveLeftVillage();
         lastDirection = 'left';
-        playRandomWalkingSound();
+      
       } else if (keyIsDown(90)) {
         moveTopVillage();
         lastDirection = 'up';
-        playRandomWalkingSound();
+        
       } else if (keyIsDown(83)) {
         moveBottomVillage();
         lastDirection = 'down';
-        playRandomWalkingSound();
       } else {
-        stopWalkingSound();
       }
     }
 
     if (!keyIsDown(68) && !keyIsDown(81) && !keyIsDown(90) && !keyIsDown(83)) {
-      stopWalkingSound();
 
       if (lastDirection === 'up') {
         updateAnimationVillage(myHeroVillageIdleTop);
